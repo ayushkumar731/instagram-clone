fa7aaab312574dc46bfa2d4dde4d06898f13e931
ayushkumar731 <kumarayush731@gmail.com>
2021-03-13 18:55:30 +0530

    fix

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,14 +5,12 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: true, // true for 465, false for other ports
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USERNAME, // generated ethereal user
         pass: process.env.EMAIL_PASSWORD, // generated ethereal password
       },
     });
-    console.log(process.env.EMAIL_HOST, transporter);
-
     const mailOptions = {
       from: `Ayush Kumar <${process.env.EMAIL_FROM}>`, // sender address
       to: options.email, // list of receivers
@@ -20,7 +18,6 @@ const sendEmail = async (options) => {
       text: options.message, // plain text body
       // html: '<b>Hello world?</b>', // html body
     };
-
     await transporter.sendMail(mailOptions);
   } catch (err) {
     console.log(err)
